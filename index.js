1ac0d5cca99e2ba9ba34910ee872841606c3b039
yosuke ota <otameshiyo23@gmail.com>
2018-05-08 22:36:54 +0900

    [add] test

--- index.js
+++ index.js
@@ -3,13 +3,16 @@
 
 const cmd = require('./lib/cmd/cmd')
 const publish = require('./lib/cmd/publish')
+const run = require('./lib/cmd/run')
 
 
 module.exports = function mrpm(opts, command, args, cb) {
   if (command === 'publish') {
-    publish(opts, args, cb)
+    return publish(opts, args, cb)
+  } else if (command === 'run') {
+    return run(opts, args, cb)
   } else {
-    cmd(opts, command, args, cb)
+    return cmd(opts, command, args, cb)
   }
 }
 
---
+++ tests/index.spec.js
@@ -0,0 +1,70 @@
+/* eslint no-sync:0, no-unused-expressions:0 */
+'use strict'
+
+const chai = require('chai')
+const {expect} = chai
+const fs = require('fs')
+const path = require('path')
+const rimraf = require('rimraf')
+const mrpm = require('../index')
+
+
+beforeEach(() => new Promise(((resolve) => {
+  rimraf(
+      path.resolve(__dirname, '../testpkg/**/*/node_modules'),
+      () => {
+        rimraf(
+            path.resolve(
+                __dirname,
+                '../testpkg/packages/*/package-lock.json'
+            ),
+            () => {
+              const opts = {
+                cwd: path.resolve(__dirname, '../testpkg')
+              }
+
+              mrpm(opts, 'i', [], (err) => {
+                expect(err).to.be.a('null')
+                resolve()
+              })
+            }
+        )
+      }
+  )
+})))
+
+describe('mrpm', () => {
+  it('should install sub-package’s dependencies', () => {
+    const stats = fs.lstatSync(
+        path.resolve(__dirname, '../testpkg/packages/sub1/node_modules')
+    )
+    const files = fs.readdirSync(
+        path.resolve(__dirname, '../testpkg/packages/sub2/node_modules')
+    )
+
+    expect(stats.isDirectory()).to.be.true
+    expect(files).to.contain('cheetah-grid')
+  })
+
+  it('should run scripts in sub-packages', () => {
+    const opts = {
+      cwd: path.resolve(__dirname, '../testpkg')
+    }
+
+    return mrpm(opts, 'run', ['test'], (err) => {
+      expect(err).to.be.a('null')
+    })
+  })
+
+  it('should fail to run scripts in sub-packages', () => {
+    const opts = {
+      cwd: path.resolve(__dirname, '../testpkg')
+    }
+
+    return mrpm(opts, 'run', ['fail'], (err) => {
+      expect(err.message).to.equal('Failure')
+      expect(err.scope).to.equal('sub2')
+      expect(err.code).to.equal(1)
+    })
+  })
+})
