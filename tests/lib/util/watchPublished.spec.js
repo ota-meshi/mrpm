375559e37cd4c927fd80b2e9888e704805664bc6
yosuke ota <otameshiyo23@gmail.com>
2018-10-01 17:32:52 +0900

    Fixed publish error

---
+++ tests/lib/util/watchPublished.spec.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const chai = require('chai')
+const expect = chai.expect
+const watchPublished = require('../../../lib/util/watchPublished')
+
+
+describe('watchPublished', () => {
+  it('watch published', () => watchPublished({
+    name: 'mrpm',
+    version: '2.0.0',
+    rootDir: process.cwd()
+  }).
+    then(() => {
+      expect(1).to.equal(1)
+    }))
+})
