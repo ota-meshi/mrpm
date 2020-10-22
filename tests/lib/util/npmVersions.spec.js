e18654d64648bd6bb92c513217c82af6c26b73e8
ota <y.ohta.z3@future.co.jp>
2020-10-22 17:57:33 +0900

    update

--- tests/lib/util/npmVersions.spec.js
+++ tests/lib/util/npmVersions.spec.js
@@ -5,23 +5,38 @@ const expect = chai.expect
 const npmVersions = require('../../../lib/util/npmVersions')
 
 
-describe('npmVersions', () => {
-  it('get versions', () => npmVersions({
-    name: 'test',
-    rootDir: process.cwd()
-  }, 'mrpm').
-    then((vers) => {
-      expect(vers).to.include.members([
-        '0.0.1',
-        '1.0.0',
-        '1.0.1'
-      ])
-    }))
-  it('404 versions', () => npmVersions({
-    name: 'test',
-    rootDir: process.cwd()
-  }, 'testtest404').
-    then((vers) => {
-      expect(vers.length).to.equal(0)
-    }))
-})
+describe(
+    'npmVersions',
+    () => {
+      it(
+          'get versions',
+          () => npmVersions(
+              {
+                name: 'test',
+                rootDir: process.cwd()
+              },
+              'mrpm'
+          ).
+            then((vers) => {
+              expect(vers).to.include.members([
+                '0.0.1',
+                '1.0.0',
+                '1.0.1'
+              ])
+            })
+      )
+      it(
+          '404 versions',
+          () => npmVersions(
+              {
+                name: 'test',
+                rootDir: process.cwd()
+              },
+              'testtest404'
+          ).
+            then((vers) => {
+              expect(vers.length).to.equal(0)
+            })
+      )
+    }
+)
